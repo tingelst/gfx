855e2fa268444dc6022399e185572b15d1545342
Lars Tingelstad <lars.tingelstad@ntnu.no>
2015-08-13 19:43:17 +0200

    Added package.xml + new CMakeLists.txt + reorganizing

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-cmake ..  
-make VERBOSE=1 -j3
--- pirun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=toolchains/pi.osx.cmake ..  
-make VERBOSE=1 -j3
-
-
-
